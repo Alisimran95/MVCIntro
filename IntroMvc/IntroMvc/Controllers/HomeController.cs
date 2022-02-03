04ba6eb59e8098dd596b563f5efe33f8bdbf62cb
Alisimran95 <alisimranem@code.edu.az>
2022-02-03 17:02:11 +0400

    HomeWork

---
+++ IntroMvc/IntroMvc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using IntroMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroMvc.Controllers
+{
+    public class HomeController :Controller
+    {
+        public IActionResult Index()
+            {
+                return View(Detail());
+        }
+
+        public IActionResult Detail()
+        {
+            var students = new List<Student>
+            {
+                new Student {Id = 1, Name = "Idris", Age = 25},
+                new Student {Id = 2,Name = "Alisimran",Age = 26}
+            };
+            return View(students);
+        }
+
+
+
+    }
+}
